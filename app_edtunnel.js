d9364d3de9c62ecaf05a5b2b158e9e8f78666b17
bili59632 <141479567+bili59632@users.noreply.github.com>
2023-08-05 14:57:05 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '07e76594-62d6-4a4b-a68d-c3a0c83e8b75';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
